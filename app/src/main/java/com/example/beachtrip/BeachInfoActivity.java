ec3229d321d48cd8c78c0432b290af17fd4dc2e4
Xiaoqian Xu <xuxiaoqi@usc.edu>
2022-10-31 20:05:16 -0700

    beach info-> beach review.

--- app/src/main/java/com/example/beachtrip/BeachInfoActivity.java
+++ app/src/main/java/com/example/beachtrip/BeachInfoActivity.java
@@ -2,7 +2,9 @@ package com.example.beachtrip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BeachInfoActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class BeachInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beach_info);
+    }
+
+    public void onClickReviews(View view) {
+        Intent intent = new Intent(this, BeachReviewActivity.class);
+        intent.putExtra("name", "beach0");
+        startActivity(intent);
     }
 }
