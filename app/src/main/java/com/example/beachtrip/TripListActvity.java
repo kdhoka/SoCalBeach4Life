fd889b5c5ea9657309415d87f5784431564c46c5
kdhoka <kdhoka@scu.edu>
2022-11-07 22:05:52 -0800

    Added trip list activity.

---
+++ app/src/main/java/com/example/beachtrip/TripListActvity.java
@@ -0,0 +1,14 @@
+package com.example.beachtrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TripListActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_list_actvity);
+    }
+}
