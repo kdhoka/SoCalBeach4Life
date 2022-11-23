0040aab5e3dad380c37655c25d9dcb472157854e
txia <xiatianrui@outlook.com>
2022-11-23 11:42:21 -0800

    adding review unit test

--- app/src/test/java/com/example/beachtrip/RestaurantUnitTest.java
+++ app/src/test/java/com/example/beachtrip/RestaurantUnitTest.java
@@ -2,6 +2,7 @@ package com.example.beachtrip;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class RestaurantUnitTest {
@@ -9,5 +10,10 @@ public class RestaurantUnitTest {
     @Test
     public void RestaurantObjectTest(){
         Restaurant rt = new Restaurant("name",null, null, new LatLng(0.0, 0.0));
+        Assert.assertEquals(rt.getName(), "name");
+        Assert.assertEquals(rt.getHours(), null);
+        Assert.assertEquals(rt.getMenu(), null);
+        LatLng target = new LatLng(0.0, 0.0);
+        Assert.assertEquals(rt.getLocation(), target);
     }
 }
